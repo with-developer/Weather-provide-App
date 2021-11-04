31672ea7bd782e98b15bf679c6a02f1ec52b30bf
건양대학교 사이버보안공학과 18학번 이준형 <50432821+with-developer@users.noreply.github.com>
2021-11-04 13:23:43 +0900

    Update main.dart

--- flutter_study/flutter_application/lib/main.dart
+++ flutter_study/flutter_application/lib/main.dart
@@ -21,11 +21,28 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Snack Bar'),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Snack Bar'),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: Builder(
+          builder: (BuildContext ctx) {
+            return Center(
+              child: FlatButton(
+                child: Text(
+                  'Show me',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  Scaffold.of(ctx).showSnackBar(SnackBar(
+                    content: Text('hello'),
+                  ));
+                },
+              ),
+            );
+          },
+        ));
   }
 }
