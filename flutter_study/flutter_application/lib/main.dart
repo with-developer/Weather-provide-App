15542d3e7408c10b06d27bdc1abcc9aa584fecca
건양대학교 사이버보안공학과 18학번 이준형 <50432821+with-developer@users.noreply.github.com>
2021-11-04 12:59:54 +0900

    Update main.dart

--- flutter_study/flutter_application/lib/main.dart
+++ flutter_study/flutter_application/lib/main.dart
@@ -22,82 +22,9 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appbar icon menu'),
+        title: Text('Snack Bar'),
         centerTitle: true,
         elevation: 0.0,
-        actions: [
-          IconButton(
-              onPressed: () {
-                print('shopping_cart button is clicked');
-              },
-              icon: Icon(Icons.shopping_cart)),
-          IconButton(
-              onPressed: () {
-                print('search button is clicked');
-              },
-              icon: Icon(Icons.search)),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/image.jpeg'),
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/image2.png'),
-                  backgroundColor: Colors.red[200],
-                )
-              ],
-              accountEmail: Text('weakness@kakao.com'),
-              accountName: Text('junhyeong'),
-              onDetailsPressed: () {
-                print('detail button is clicked');
-              },
-              decoration: BoxDecoration(
-                  color: Colors.red[200],
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0))),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: () {
-                print('Home Button is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('Settings'),
-              onTap: () {
-                print('Settings Button is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('Q&A'),
-              onTap: () {
-                print('Q&A Button is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
       ),
     );
   }
