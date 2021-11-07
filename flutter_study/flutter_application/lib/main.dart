0be173a9b9c6e3a6cb70014cd92df92dc51aa4b5
건양대학교 사이버보안공학과 18학번 이준형 <50432821+with-developer@users.noreply.github.com>
2021-11-08 02:09:10 +0900

    main

--- flutter_study/flutter_application/lib/main.dart
+++ flutter_study/flutter_application/lib/main.dart
@@ -1,22 +1,61 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-import 'settings.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Introduction',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Home(),
-        '/settings': (context) => Settings(),
-      },
+      title: 'Dice Game',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Center(
+            child: const Image(
+              image: const AssetImage('image/chef.gif'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
