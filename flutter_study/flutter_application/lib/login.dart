83a916ad191a7942efec1f784771a42ade22c2b0
건양대학교 사이버보안공학과 18학번 이준형 <50432821+with-developer@users.noreply.github.com>
2021-11-09 02:35:15 +0900

    finish

--- flutter_study/flutter_application/lib/login.dart
+++ flutter_study/flutter_application/lib/login.dart
@@ -33,87 +33,93 @@ class _LogInState extends State<LogIn> {
       ),
       body: Builder(
         builder: (context) {
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                const Padding(padding: EdgeInsets.only(top: 50)),
-                const Center(
-                  child: Image(
-                    image: AssetImage('assets/image2.png'),
-                    width: 170.0,
-                    height: 190.0,
+          return GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.only(top: 50)),
+                  const Center(
+                    child: Image(
+                      image: AssetImage('assets/image2.png'),
+                      width: 170.0,
+                      height: 190.0,
+                    ),
                   ),
-                ),
-                Form(
-                  child: Theme(
-                    data: ThemeData(
-                      primaryColor: Colors.teal,
-                      inputDecorationTheme: const InputDecorationTheme(
-                        labelStyle: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 15.0,
+                  Form(
+                    child: Theme(
+                      data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: const InputDecorationTheme(
+                          labelStyle: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ),
+                      child: Container(
+                        padding:
+                            const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 0.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                                controller: controller,
+                                decoration: const InputDecoration(
+                                  labelText: 'ID',
+                                ),
+                                keyboardType: TextInputType.emailAddress),
+                            TextField(
+                              controller: controller2,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                            ),
+                            const SizedBox(
+                              height: 40.0,
+                            ),
+                            ButtonTheme(
+                              minWidth: 100.0,
+                              height: 50.0,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.orangeAccent,
+                                ),
+                                onPressed: () {
+                                  if (controller.text == 'admin' &&
+                                      controller2.text == 'admin') {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (BuildContext context) =>
+                                                const Dice()));
+                                  } else if (controller.text != 'admin' &&
+                                      controller2.text == 'admin') {
+                                    floating(context, '아이디가 일치하지 않습니다.');
+                                  } else if (controller.text == 'admin' &&
+                                      controller2.text != 'admin') {
+                                    floating(context, '패스워드가 일치하지 않습니다.');
+                                  } else {
+                                    floating(context, '로그인 정보를 다시 확인하세요.');
+                                  }
+                                },
+                                child: const Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 35.0,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 0.0),
-                      child: Column(
-                        children: [
-                          TextField(
-                              controller: controller,
-                              decoration: const InputDecoration(
-                                labelText: 'ID',
-                              ),
-                              keyboardType: TextInputType.emailAddress),
-                          TextField(
-                            controller: controller2,
-                            decoration: const InputDecoration(
-                              labelText: 'Password',
-                            ),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          const SizedBox(
-                            height: 40.0,
-                          ),
-                          ButtonTheme(
-                            minWidth: 100.0,
-                            height: 50.0,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.orangeAccent,
-                              ),
-                              onPressed: () {
-                                if (controller.text == 'admin' &&
-                                    controller2.text == 'admin') {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (BuildContext context) =>
-                                              const Dice()));
-                                } else if (controller.text != 'admin' &&
-                                    controller2.text == 'admin') {
-                                  floating(context, '아이디가 일치하지 않습니다.');
-                                } else if (controller.text == 'admin' &&
-                                    controller2.text != 'admin') {
-                                  floating(context, '패스워드가 일치하지 않습니다.');
-                                } else {
-                                  floating(context, '로그인 정보를 다시 확인하세요.');
-                                }
-                              },
-                              child: const Icon(
-                                Icons.arrow_forward,
-                                color: Colors.white,
-                                size: 35.0,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           );
         },
