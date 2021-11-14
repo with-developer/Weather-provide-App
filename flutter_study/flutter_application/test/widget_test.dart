56b41f35d469b1b8b7d2d68ca400a49fb0ca3ae8
건양대학교 사이버보안공학과 18학번 이준형 <50432821+with-developer@users.noreply.github.com>
2021-11-15 03:58:20 +0900

    widget_test

--- flutter_study/flutter_application/test/widget_test.dart
+++ flutter_study/flutter_application/test/widget_test.dart
@@ -1,30 +1,28 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_application/main.dart';
+import 'dart:io';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  showData();
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+void showData() {
+  startTask();
+  accessData();
+  fecthData();
+}
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+void startTask() {
+  String info1 = '요청수행 시작';
+  print(info1);
+}
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+void accessData() {
+  Duration time = const Duration(seconds: 3);
+  sleep(time);
+  String info2 = '데이터에 접속중';
+  print(info2);
+}
+
+void fecthData() {
+  String info3 = '잔액은 8,500원 입니다';
+  print(info3);
 }
