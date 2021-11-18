a51a2b521e2c3f5e0c19fb9d5fd05b78ab7cbef7
건양대학교 사이버보안공학과 18학번 이준형 <50432821+with-developer@users.noreply.github.com>
2021-11-18 20:01:16 +0900

    update

--- flutter_study/flutter_application/lib/screens/weather_screen.dart
+++ flutter_study/flutter_application/lib/screens/weather_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/instance_manager.dart';
 import 'package:get/get.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({Key? key}) : super(key: key);
@@ -31,29 +33,67 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '${cityName}',
-              style: TextStyle(
-                fontSize: 30.0,
-              ),
+        backgroundColor: Colors.amber[300],
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text(''),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.near_me,
+              color: Colors.black,
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              '${int_temp} °C',
-              style: TextStyle(
-                fontSize: 30.0,
+            onPressed: () {},
+            iconSize: 30.0,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.location_searching,
+                color: Colors.black,
               ),
-            )
+              iconSize: 30.0,
+            ),
           ],
         ),
-      ),
-    ));
+        body: Container(
+          child: Stack(
+            children: [
+              // Image.asset(
+              //   'image/background.jpg',
+              //   height: double.infinity,
+              //   width: double.infinity,
+              //   fit: BoxFit.cover,
+              // )
+            ],
+          ),
+        ));
   }
 }
+
+// body: SafeArea(
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 '${cityName}',
+//                 style: TextStyle(
+//                   fontSize: 30.0,
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 20.0,
+//               ),
+//               Text(
+//                 '${int_temp} °C',
+//                 style: TextStyle(
+//                   fontSize: 30.0,
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
